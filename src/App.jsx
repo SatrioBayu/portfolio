16479e510ef2f9ceeade15c4ea54ef399037a967
unknown <sbayu875@gmail.com>
2025-11-07 11:09:03 +0800

    feat: import index.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Achievements from "./components/Achievements";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./styles/index.css";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <Experience />
       <Achievements />
       <Contact />
+      <Footer />
     </>
   );
 }
